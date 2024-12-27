4dc82f1ce90a79e4ddeba38a3d05a911c06453f2
azamjonbro <azamjonbro@gmail.com>
2024-12-27 16:55:16 +0500

    edited middleware

--- validations/user.validation.js
+++ validations/user.validation.js
@@ -2,21 +2,15 @@
 const Joi = require("joi");
 
 const userValidationSchema = Joi.object({
-    firstname: Joi.string().min(3).max(50),
-    lastname: Joi.string().required(),
-    age: Joi.number().required(),
     username: Joi.string().required(),
     password: Joi.string().required(),
-    birthday: Joi.string(),
+    email: Joi.string(),
 })
 
 const updateUserValidationSchema = Joi.object({
-    firstname: Joi.string().trim().min(5).max(50),
-    lastname: Joi.string().required(),
-    age: Joi.number().required(),
     username: Joi.string().required(),
     password: Joi.string().required(),
-    birthday: Joi.string(),
+    email: Joi.string(),
 })
 
 
